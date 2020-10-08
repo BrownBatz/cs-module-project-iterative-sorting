d6f280173f6b2737715d811c3d49f17b2c689d84
BrownBatz <lopeztgabriel4@gmail.com>
2020-10-07 20:44:40 -0600

    I have hit MVP for this project

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,6 +1,14 @@
 def linear_search(arr, target):
     # Your code here
-
+    length = len(arr)
+    tracker = 0
+    if length > 1:
+        while tracker < length:
+            if arr[tracker] == target:
+                return tracker
+            else:
+                tracker += 1
+                continue
 
     return -1   # not found
 
@@ -9,6 +17,14 @@ def linear_search(arr, target):
 def binary_search(arr, target):
 
     # Your code here
-
-
+    low = 0
+    high = len(arr) - 1
+    while low <= high:
+        middle = round((low+high)/2)
+        if target < arr[middle]:
+            high = middle - 1
+        elif target > arr[middle]:
+            low = middle + 1
+        else:
+            return middle
     return -1  # not found
